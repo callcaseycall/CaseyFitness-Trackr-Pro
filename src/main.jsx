32a7b2815ec8b429c346bbc520a4bddc4b4f5fbc
callcaseycall <callcaseycall@gmail.com>
2025-08-20 21:27:07 -0600

    First commit, errors I need to figure out

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,27 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Layout from "./layout/Layout";
+import Layout from "..src/layout/Layout.jsx";
 
 import { AuthProvider } from "./auth/AuthContext";
 import { ApiProvider } from "./api/ApiContext";
-import { PageProvider } from "./layout/PageContext";
+// import { PageProvider } from "./layout/PageContext";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Navbar from "./layout/Navbar.jsx";
+import Register from "./auth/Register.jsx";
+import Login from "./auth/Login.jsx";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <ApiProvider>
-      <PageProvider>
-        <Layout>
-          <App />
-        </Layout>
-      </PageProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<Layout />}></Route>
+          <Route path="/Navbar" element={<Navbar />}></Route>
+          <Route path="/Register" element={<Register />}></Route>
+          <Route path="/Login/:id" element={<Login />}></Route> */}
+        </Routes>
+      </BrowserRouter>
     </ApiProvider>
-  </AuthProvider>,
+  </AuthProvider>
 );
